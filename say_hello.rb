5b4abfe184b2442a30dab0ac1649b72c7895dbbf
Valeria Johnson <valeria.johnson16@gmail.com>
2020-06-18 00:40:48 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,58 @@
-# Build your say_hello method here
+
+ def say_hello
+   puts "Hello Gabriela"
+ end
+ 
+ say_hello
+ 
+ def say_hello
+   puts "Hello Ruby Programmer!"
+ end
+ 
+ say_hello
+ 
+def say_hello
+  puts "Hello Kent Beck!"
+end
+
+say_hello
+
+def say_hello(name="Gabriela")
+  puts "Hello, #{name}!"
+end
+
+def say_hello(name="Gabriela")
+  puts "Hello, #{name}!"
+end
+
+
+
+
+
+say_hello
+
+def say_hello(name="Ruby Programmer!")
+  puts "Hello, #{name}!"
+end
+
+say_hello
+
+def say_hello(name="Kent Beck!")
+  puts "Hello, #{name}!"
+end
+
+say_hello  
+
+class Methods
+  def self.hello(player)
+    print "Hello, " << player
+  end
+  hello("Annie")
+end
+
+
+ 
+
+
+
+
