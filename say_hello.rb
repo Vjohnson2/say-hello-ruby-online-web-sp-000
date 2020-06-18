818b0e48278f370484ecafca3061464164e6e6bc
Valeria Johnson <valeria.johnson16@gmail.com>
2020-06-18 01:02:51 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,54 +1,18 @@
 
- def say_hello
-   puts "Hello Gabriela"
- end
  
- say_hello
- 
- def say_hello
-   puts "Hello Ruby Programmer!"
- end
- 
- say_hello
- 
-def say_hello
-  puts "Hello Kent Beck!"
-end
-
-say_hello
-
-def say_hello(name="Gabriela")
-  puts "Hello, #{name}!"
-end
-
-def say_hello(name="Gabriela")
-  puts "Hello, #{name}!"
-end
 
 
 
 
 
-say_hello
+
+
 
 def say_hello(name="Ruby Programmer!")
   puts "Hello, #{name}!"
 end
 
-say_hello
 
-def say_hello(name="Kent Beck!")
-  puts "Hello, #{name}!"
-end
-
-say_hello  
-
-class Methods
-  def self.hello(player)
-    print "Hello, " << player
-  end
-  hello("Annie")
-end
 
 
  
